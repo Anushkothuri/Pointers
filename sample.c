8aec6d31665b23bc955bc59af8f498bcf61341e3
Anushkothuri <kothurianush16@gmail.com>
2024-05-17 23:44:33 +0530

    sample

---
+++ sample.c
@@ -0,0 +1,13 @@
+// Basic idea on pointers.
+#include<stdio.h>
+int main()
+{
+    int x=10;
+    int *p;
+    p=&x;
+    printf("%d\n",x);
+    printf("%d\n",*p);
+    printf("%p\n",p);
+    printf("%p\n",&p);
+    return 0;
+}
