0338d4e135deaa55529cd02b2f72f829199da407
Anushkothuri <kothurianush16@gmail.com>
2024-05-29 22:08:37 +0530

    cmmnts

--- sample.c
+++ sample.c
@@ -5,9 +5,9 @@ int main()
     int x=10;
     int *p;
     p=&x;
-    printf("%d\n",x);
-    printf("%d\n",*p);
-    printf("%p\n",p);
-    printf("%p\n",&p);
+    printf("%d\n",x);  // prints value of  'x'.
+    printf("%d\n",*p); // prints return value of 'p' that means 'x' value.
+    printf("%p\n",p);  // prints address of 'x'.
+    printf("%p\n",&p); // prints address of pointer.
     return 0;
 }
